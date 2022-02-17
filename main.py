0fea77f0979991994f627d45b66819b24858bb1d
Udit <uditmishra.um@gmail.com>
2022-02-17 20:28:29 +0530

    Snake-Game Start Code- Created a snake with black background.

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+start_point = 0
+for _ in range(3):
+    snake = Turtle("square")
+    snake.color("white")
+    snake.goto(x= start_point, y=0)
+    start_point = start_point -20
+
+print(snake.position())
+
+screen.exitonclick()
