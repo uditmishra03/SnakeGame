0f0f62166ff9f34bd90ed3e3be2c6160106d74f2
Udit <uditmishra.um@gmail.com>
2022-02-19 15:17:59 +0530

    Snake Head- Added movements of snake as well as SnakeClass for the creation of snake at the start of the game.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-start_point = 0
-for _ in range(3):
-    snake = Turtle("square")
-    snake.color("white")
-    snake.goto(x= start_point, y=0)
-    start_point = start_point -20
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
 
 
 screen.exitonclick()
