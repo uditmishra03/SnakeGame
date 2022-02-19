64248a08eefc3f28145f29f242340d135627a14b
Udit <uditmishra.um@gmail.com>
2022-02-19 17:30:54 +0530

    Added Food.py and scoreboard.py for food collision and scoreboard ticking.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -18,12 +22,18 @@ screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    snake.move()
+    #Detect collision with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.current_score += 1
+        scoreboard.update_score()
 
+    snake.move()
 
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.goto(-30, 280)
+        self.current_score = 0
+        self.update_score()
+
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score:{self.current_score} ", move= False, align="center",  font=("Courier New", 12, "bold"))
